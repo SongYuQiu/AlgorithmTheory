9e5ee25df0733e2ffd6583a63d45a6fbd096386a
JustforStar <1594562760@qq.com>
2021-11-22 19:32:37 +0800

    problem2

---
+++ problem2-二分法找凸多边形坐标最大点/problem2-二分法找凸多边形坐标最大点/problem2-二分法找凸多边形坐标最大点.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  problem2-二分法找凸多边形坐标最大点
+//
+//  Created by JustforStar on 2021/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
